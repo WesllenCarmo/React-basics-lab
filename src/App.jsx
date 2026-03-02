d1361850e7bc193d386447e075132c2e11540e71
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-03-01 18:10:59 -0800

    Add LabelButton component with prop and onClick function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import LabelButton from './components/LabelButton'
 
 function App() {
-
+  const handleClick = () => {
+    alert("Button clicked from App!");
+  }
   return (
     <>
-      
+      <LabelButton onClick={handleClick} label="Click Me" />
+      <p>The component uses onClick function and receive the prop label's value.</p>
     </>
   )
 }
